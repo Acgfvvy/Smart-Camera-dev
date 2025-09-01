f217c51bbc879d6bf09d8306467f07390794bc2e
Abhyansh Lodhi <omsjsr@outlook.com>
2025-09-01 19:46:32 +0530

    Init
    Signed-off-by: Abhyansh Lodhi <omsjsr@outlook.com>

---
+++ source/app/src/main/java/com/gmail/omsjsr/smartcamera/DelegationService.java
@@ -0,0 +1,15 @@
+package com.gmail.omsjsr.smartcamera;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    /** @noinspection EmptyMethod*/
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
